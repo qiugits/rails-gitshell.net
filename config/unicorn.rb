2df5457176dfd8c78c58eb56e71edff06926c6f4
qiu <qiu@MorganaApache.local>
2017-02-24 05:23:36 +0900

    first commit

---
+++ config/unicorn.rb
@@ -0,0 +1,13 @@
+#Unicornのログ出力先を指定
+#File.expand_pathはパスを絶対パスに解決するメソッド File.expand_path: https://ruby-doc.org/core-2.2.0/File.html#method-c-expand_path
+rails_root = File.expand_path('../../', __FILE__) #=> "/Users/qiu/workspace/qiu.jp"
+#ワーカー数を指定する
+worker_processes 2
+working_directory rails_root
+
+listen "#{rails_root}/tmp/unicorn.sock" #？？「Unicornのプロセスをlistenするアドレストポートを指定」らしいが…
+pid "#{rails_root}/tmp/unicorn.pid" #pid fileの位置を指定する
+
+#ログの出力先を指定
+stderr_path "#{rails_root}/log/unicorn_error.log"
+stdout_path "#{rails_root}/log/unicorn.log"
