b3204d1c4da7f7b4f2b041cc0f8faa1fb1626835
Qiushi Pan <qiushi@Alita.local>
2017-02-26 04:25:58 +0900

    deleted scaffold:tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :profile
 
-  resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root :to => 'profile#index'
 end
