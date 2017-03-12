b4698d0805c4c65d68084c27ef78183a87a799e9
Qiushi Pan <qiushi@Alita.local>
2017-03-12 17:30:05 +0900

    sh script update

--- shell_scripts/server_production.sh
+++ shell_scripts/server_production.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
-cd /Users/qiu/workspace/qiu.jp
+#cd /Users/qiu/workspace/qiu.jp
 pwd
-#echo "x restart apache"
-#apachectl restart
 echo "////////////////////////////////////////"
 echo " * restart unicorn.rb"
 echo "  - stop unicorn"
 kill -QUIT `cat ~/workspace/qiu.jp/tmp/pids/unicorn.pid`
-#kill -QUIT `cat ~/workspace/qiu.jp/tmp/unicorn.pid` #どちらの場合も想定して置く
 echo "  - start unicorn as daemon"
-#echo "development"
-#unicorn_rails -c ~/workspace/qiu.jp/config/unicorn.rb -E development -D
 echo "   *__production__*"
 unicorn_rails -c ~/workspace/qiu.jp/config/unicorn.rb -E production -D
 echo "////////////////////////////////////////"
@@ -21,6 +16,4 @@ echo "  - stop"
 nginx -s stop
 echo "  - start"
 nginx
-#echo "  restart nginx  "
-#nginx -s reload
 echo "////////////////////////////////////////"
