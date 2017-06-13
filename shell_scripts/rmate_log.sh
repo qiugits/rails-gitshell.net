5f88094acc9689f792b1bb39bd40301c1ed0fb77
Qiu <qiu.gits@gmail.com>
2017-06-13 16:04:12 +0900

    small change

--- shell_scripts/rmate_log.sh
+++ shell_scripts/rmate_log.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 echo "////////////////////////////////////////"
-echo "rmate nginx qiu.jp_access.log"
-rmate -f /usr/local/var/log/nginx/qiu.jp_access.log
-echo "rmate nginx qiu.jp_access.log"
-rmate -f /usr/local/var/log/nginx/qiu.jp_error.log
-echo "rmate unicorn qiu.jp access log"
-rmate -f /Users/qiu/workspace/qiu.jp/log/unicorn.log
-echo "rmate unicorn qiu.jp error log"
-rmate -f /Users/qiu/workspace/qiu.jp/log/unicorn_error.log
+echo "rmate nginx gitshell.net_access.log"
+rmate -f /usr/local/var/log/nginx/gitshell.net_access.log
+echo "rmate nginx gitshell.net_access.log"
+rmate -f /usr/local/var/log/nginx/gitshell.net_error.log
+echo "rmate unicorn gitshell.net access log"
+rmate -f /Users/qiu/workspace/gitshell.net/log/unicorn.log
+echo "rmate unicorn gitshell.net error log"
+rmate -f /Users/qiu/workspace/gitshell.net/log/unicorn_error.log
 echo "////////////////////////////////////////"
