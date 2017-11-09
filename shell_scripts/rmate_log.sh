aca6505e237520cdf2e6219ba79a141a027e19a5
Qiu <qiu.gits@gmail.com>
2017-11-10 04:26:02 +0900

    Update rmate_log.sh

--- shell_scripts/rmate_log.sh
+++ shell_scripts/rmate_log.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-echo "////////////////////////////////////////"
-echo "rmate nginx gitshell.net_access.log"
+
+# nginx
+# access log
 rmate -f /usr/local/var/log/nginx/gitshell.net_access.log
-echo "rmate nginx gitshell.net_access.log"
+# error log
 rmate -f /usr/local/var/log/nginx/gitshell.net_error.log
-echo "rmate unicorn gitshell.net access log"
+
+# unicorn
+# access log
 rmate -f /Users/qiu/workspace/gitshell.net/log/unicorn.log
-echo "rmate unicorn gitshell.net error log"
+# error log
 rmate -f /Users/qiu/workspace/gitshell.net/log/unicorn_error.log
-echo "////////////////////////////////////////"
